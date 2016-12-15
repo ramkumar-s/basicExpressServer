75d63b801268adf3d5dd2c60b4b2720a63af5ebc
Ramkumar <ram.9107@gmail.com>
2016-12-15 17:54:21 +0530

    updated app.js
    Fixed error in line 5.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var config = require('./config');
 var app = express();
 
-app.use(express.static('public'));
+app.use(express.static(__dirname+'/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(config.indexFile);
